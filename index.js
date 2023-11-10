50df129cff62affdfe59c9615653fdd8c527fdf6
andashevabegimai <118152382+andashevabegimai@users.noreply.github.com>
2023-11-10 19:35:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 import { add as binaryAdd, isValid as binaryIsValid } from './binary.js';
 import { add as hexAdd, isValid as hexIsValid } from './hex.js';
 
+const isValidMap = {
+   bin: binaryIsValid,
+   hex: hexIsValid,
+};
+
 var type = prompt(`Addition type ('bin' for binary; 'hex' for hexadecimal):`);
 
 if (type !== 'bin' && type !== 'hex') {
    alert('Invalid addition type.');
 }
 else {
-   // The type is valid.
+   var isValid = isValidMap[type];
+   var a = prompt('Enter number 1:');
+   if (!isValid(a)) {
+      alert('Invalid number entered!');
+   }
+   else {
+      var b = prompt('Enter number 2:');
+      if (!isValid(b)) {
+         alert('Invalid number entered!');
+      }
+      else {
+         // Everything's fine.
+      }
+   }
 }
