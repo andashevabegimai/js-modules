70a668e4ad9ebf138620a5ad8a504adb9cfa21f4
andashevabegimai <118152382+andashevabegimai@users.noreply.github.com>
2023-11-10 19:33:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 import { add as binaryAdd } from './binary.js';
 import { add as hexAdd } from './hex.js';
+
+var type = prompt(`Addition type ('bin' for binary; 'hex' for hexadecimal):`);
+
+if (type !== 'bin' && type !== 'hex') {
+   alert('Invalid addition type.');
+}
+else {
+   // The type is valid.
+}
